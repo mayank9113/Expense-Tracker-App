90a4f3a27c1241aa98ea319f6e1a70f0d206831c
Mayank Prajapati <MAYANKPRAJAPATI.1999@gmail.com>
2023-07-23 12:14:46 +0530

    v2

---
+++ dbConnect.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
+
+mongoose.connect( (process.env.MONGO_URL),
+    {useNewUrlParser : true , useUnifiedTopology : true})
+
+const connection = mongoose.connection
+
+connection.on('error', err => console.log(err))
+
+connection.on('connected', () => console.log('Mongo DB Connection Successfull'))
